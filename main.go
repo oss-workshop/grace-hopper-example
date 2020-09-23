2d615a320ec6684d633170fcef4e588487965833
OSS Contributor <oss.contributor@gmail.com>
2020-09-23 13:07:41 -0700

    feat: Display custom message

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import "fmt"
 const name = "Grace Hopper"
 
 func main() {
-	fmt.Println(message())
+	fmt.Println(customMessage("This is my custom message"))
 }
 
 func message() string {
 	return fmt.Sprintf("Open Source ♡ %s!", name)
+}
+
+func customMessage(message string) string {
+	return message
 }
